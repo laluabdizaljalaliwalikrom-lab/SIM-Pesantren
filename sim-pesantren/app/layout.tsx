0bc0eee1cc16007e13a6e9a8c762062e0425db59
laluabdizaljalaliwalikrom <laluabdizaljalaliwalikrom@gmail.com>
2026-06-02 00:38:27 +0800

    feat: add Vercel Speed Insights for performance monitoring

--- sim-pesantren/app/layout.tsx
+++ sim-pesantren/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 import { Toaster } from "sonner";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -69,6 +70,7 @@ export default async function RootLayout({
           {children}
           <Toaster position="top-right" richColors />
           <Analytics />
+          <SpeedInsights />
         </ThemeProvider>
       </body>
     </html>
